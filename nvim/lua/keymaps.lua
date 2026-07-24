65bb1caeb0a8c3a1636314c18ad72dfe4d2cffff
moosch <mail@moosch.io>
2026-07-24 12:36:05 +0930

    Move selected line added and colour change

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -22,4 +22,7 @@ end, { desc = "Close buffer without breaking layout" })
 vim.keymap.set("n", "<leader><", "<C-o>", { desc = "Jump back" })
 vim.keymap.set("n", "<leader>>", "<C-i>", { desc = "Jump forward" })
 
+vim.keymap.set("v", "<S-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "<S-Up>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
